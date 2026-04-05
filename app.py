03d285f0be9f12ca2c190b51a12215d614f86740
riteshdresfinity-hash <ritesh.dresfinity@gmail.com>
2026-04-05 18:13:11 +0530

    app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ from flask import (Flask, render_template_string, request, redirect,
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', secrets.token_hex(32))
 app.permanent_session_lifetime = timedelta(days=365)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "database.db")
+DB_PATH = 'budget.db'
 FLASK_URL = 'http://127.0.0.1:5000'
 
 # ─────────────────────────── DATABASE ────────────────────────────
